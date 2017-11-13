0ee6bcd30405fa91af852e5e315205995cc757e2
snoopy-_-max@hotmail.com <512060925@qq.com>
2017-11-13 18:08:41 +0800

    'test'

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+body{
+  position: relative;
+  overflow-x: hidden;
+  font-size: 24rem;
+}
+#app {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  min-height: 98rem;
+  max-height: 115rem;
+  max-width: 64rem;
+  margin: 0 auto;
+  /*background: url(/gold/static/img/bg.2abc21f.jpg) no-repeat 50%;*/
+  background-size: cover;
+  overflow: hidden;
+}
+</style>
